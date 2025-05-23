bc51c0e129f8a31311b96b899222ec0f07501723
Rohit <dv6510@gmail.com>
2025-05-24 03:50:04 +0530

    more configs

--- init.lua
+++ init.lua
@@ -15,5 +15,6 @@ vim.opt.rtp:prepend(lazypath)
 require("vim-options")
 require("lazy").setup("plugins")
 
+vim.o.splitright = true
 
 
--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -5,25 +5,26 @@ return {
       { "github/copilot.vim" },
       { "nvim-lua/plenary.nvim", branch = "master" },
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
+    build = "make tiktoken",  -- MacOS/Linux only
     opts = {
       window = {
-        layout = "vertical", -- vertical split
-        position = "right",  -- set to "right" to open on right side
-        width = 0.4,         -- optional: control width as a fraction (0.4 = 40%)
+        layout   = "vertical",  -- 'float' | 'vertical' | 'horizontal'
+        position = "right",     -- only for vertical
+        width    = 0.4,         -- 40% of editor width
       },
     },
     keys = {
-      { "<Leader>zc", ":CopilotChat<CR>",           mode = "n", desc = "Chat with Copilot" },
-      { "<Leader>ze", ":CopilotChatExplain<CR>",    mode = "v", desc = "Explain Code" },
-      { "<Leader>zr", ":CopilotChatReview<CR>",     mode = "v", desc = "Review Code" },
-      { "<Leader>zf", ":CopilotChatFix<CR>",        mode = "v", desc = "Fix Code Issues" },
-      { "<Leader>zo", ":CopilotChatOptimize<CR>",   mode = "v", desc = "Optimize Code" },
-      { "<Leader>zd", ":CopilotChatDocs<CR>",       mode = "v", desc = "Generate Docs" },
-      { "<Leader>zt", ":CopilotChatTests<CR>",      mode = "v", desc = "Generate Tests" },
-      { "<Leader>zm", ":CopilotChatCommit<CR>",     mode = "n", desc = "Generate Commit Message" },
-      { "<Leader>zs", ":CopilotChatCommit<CR>",     mode = "v", desc = "Generate Commit for Selection" },
+      { "<Leader>zc", ":CopilotChat<CR>",        mode = "n", desc = "Chat with Copilot" },
+      { "<Leader>ze", ":CopilotChatExplain<CR>", mode = "v", desc = "Explain Code" },
+      { "<Leader>zr", ":CopilotChatReview<CR>",  mode = "v", desc = "Review Code" },
+      { "<Leader>zf", ":CopilotChatFix<CR>",     mode = "v", desc = "Fix Code Issues" },
+      { "<Leader>zo", ":CopilotChatOptimize<CR>",mode = "v", desc = "Optimize Code" },
+      { "<Leader>zd", ":CopilotChatDocs<CR>",    mode = "v", desc = "Generate Docs" },
+      { "<Leader>zt", ":CopilotChatTests<CR>",   mode = "v", desc = "Generate Tests" },
+      { "<Leader>zm", ":CopilotChatCommit<CR>",  mode = "n", desc = "Commit Message" },
+      { "<Leader>zs", ":CopilotChatCommit<CR>",  mode = "v", desc = "Commit for Selection" },
     },
   },
 }
+
 
