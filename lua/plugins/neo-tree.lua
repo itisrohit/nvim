3d4fbb6ab37f8caef59efdbe7c37029e660e8483
Rohit <dv6510@gmail.com>
2025-07-11 22:59:05 +0530

    updated settings to show hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,24 @@
-return  {
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,        -- Show hidden files by default
+          hide_dotfiles = false, -- Explicitly show dotfiles (like .env)
+          hide_gitignored = false, -- Show files normally ignored by git
+        },
+      },
+    })
+
+    -- Keymap: Toggle Neo-tree on the left
+    vim.keymap.set('n', '<C-n>', '<Cmd>Neotree toggle left<CR>', { desc = "Toggle file tree (neo-tree)" })
+  end,
 }
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,17 +5,22 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+      -- Show hidden files in file finder (e.g. .env, .gitignore)
+      vim.keymap.set('n', '<C-p>', function()
+        builtin.find_files({ hidden = true })
+      end, { desc = "Telescope: Find files (incl. dotfiles)" })
+
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope: Live grep" })
     end
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-      require("telescope").setup ({
+      require("telescope").setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
+            require("telescope.themes").get_dropdown({})
           }
         }
       })
@@ -24,3 +29,4 @@ return {
     end
   },
 }
+
